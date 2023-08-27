c1e5466de20b79e453b5394433fab0990b817214
Mudassar <mudassar.elley@gmail.com>
2023-08-27 19:56:17 +0500

    a bit text tweaks

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -55,7 +55,7 @@ export default function ArticlesIndex({ articles }) {
 
 ArticlesIndex.layoutProps = {
   meta: {
-    title: 'Articles',
+    title: 'Blog',
     description:
       'All of my long-form thoughts on programming, leadership, product design and development, and more, collected in chronological order.',
   },
